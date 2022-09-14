6be303f3aca50c403489fcdcc7aef36800e9ca01
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-09-14 15:40:56 -0400

    Adiciona um texto centralizado ao header.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const email = document.getElementById('email');
-console.log(email)
-const password = document.getElementById('password');
+const email = document.getElementById("email");
+console.log(email);
+const password = document.getElementById("password");
 
-const buttonLogin = document.querySelector('.buttonEntrar');
+const buttonLogin = document.querySelector(".buttonEntrar");
 
-function login(){
-    if (email.value === 'tryber@teste.com' && password.value ==='123456'){
-        window.alert('Olá, Tryber!');
-    }else{
-        window.alert('Email ou senha inválidos.');
-    }
-
+function login() {
+  if (email.value === "tryber@teste.com" && password.value === "123456") {
+    window.alert("Olá, Tryber!");
+  } else {
+    window.alert("Email ou senha inválidos.");
+  }
 }
 
-buttonLogin.addEventListener('click', login);
+buttonLogin.addEventListener("click", login);
