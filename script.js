d828d7106261a1620defcba6fc6c72ecbc2a93db
Lucas <lucasramoslobo83@gmail.com>
2022-09-14 16:30:18 -0300

    Criando o formulário de login e senha

---
+++ script.js
@@ -0,0 +1,16 @@
+const email = document.getElementById('email');
+console.log(email)
+const password = document.getElementById('password');
+
+const buttonLogin = document.querySelector('.buttonEntrar');
+
+function login(){
+    if (email.value === 'tryber@teste.com' && password.value ==='123456'){
+        window.alert('Olá, Tryber!');
+    }else{
+        window.alert('Email ou senha inválidos.');
+    }
+
+}
+
+buttonLogin.addEventListener('click', login);
