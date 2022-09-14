8d0da73e6836258e7f83912e9b560c9a4660d870
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-09-14 19:06:04 -0400

    Adiciona footer.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const email = document.getElementById('email');
+const email = document.getElementById("email");
 console.log(email);
-const password = document.getElementById('password');
+const password = document.getElementById("password");
 
-const buttonLogin = document.querySelector('.buttonEntrar');
+const buttonLogin = document.querySelector(".buttonEntrar");
 
 function login() {
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
-    window.alert('Olá, Tryber!');
+  if (email.value === "tryber@teste.com" && password.value === "123456") {
+    window.alert("Olá, Tryber!");
   } else {
-    window.alert('Email ou senha inválidos.');
+    window.alert("Email ou senha inválidos.");
   }
 }
 
+document.getElementById("submit-btn").disabled = true;
+document
+  .getElementById("agreement")
+  .addEventListener("click", function (event) {
+    let conteudo = document.getElementById("agreement").value;
+    if (conteudo !== null && conteudo !== "") {
+      document.getElementById("submit-btn").disabled = false;
+    } else {
+      document.getElementById("submit-btn").disabled = true;
+    }
+  });
 
-document.getElementById('submit-btn').disabled = true;
-document.getElementById('agreement').addEventListener('click', function(event) {
-  let conteudo = document.getElementById("agreement").value;
-  if (conteudo !== null && conteudo !== '') {
-    document.getElementById("submit-btn").disabled = false;
-  } else { 
-    document.getElementById("submit-btn").disabled = true;
-  }
-});
-
-buttonLogin.addEventListener('click', login);
+buttonLogin.addEventListener("click", login);
